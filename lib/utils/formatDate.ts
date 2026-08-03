b92d59b8cbb5d7899b5016a7333dd26db836cf0a
RianRahmanAlaziz <rianrahman8243@gmail.com>
2026-08-03 10:47:00 +0700

    refactor(lib): formatDate

--- lib/utils/formatDate.ts
+++ lib/utils/formatDate.ts
@@ -5,12 +5,30 @@ export function formatDate(
         return "-";
     }
 
-    return new Date(date).toLocaleDateString(
+    const formatter = new Intl.DateTimeFormat(
         "en-GB",
         {
             day: "2-digit",
             month: "long",
             year: "numeric",
         },
-    ).replace(/ /g, "-");
+    );
+
+    const parts = formatter.formatToParts(
+        new Date(date),
+    );
+
+    const day = parts.find(
+        (part) => part.type === "day",
+    )?.value ?? "";
+
+    const month = parts.find(
+        (part) => part.type === "month",
+    )?.value ?? "";
+
+    const year = parts.find(
+        (part) => part.type === "year",
+    )?.value ?? "";
+
+    return `${day} ${month} ${year}`;
 }
